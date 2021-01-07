21b7c1b98ebe1f3d4699867857eab0814ca7d93f
Tzanio <tzanio@llnl.gov>
2021-01-06 16:11:44 -0800

    trying glvis.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,14 @@
+// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-443271.
+//
+// This file is part of the GLVis visualization tool and library. For more
+// information and source code availability see https://glvis.org.
+//
+// GLVis is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
 function openFullscreen(elem) {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
@@ -73,4 +84,16 @@ function augmentLoggers(log_id, log_cont_id) {
   ["log", "warn", "error", "info", "debug"].forEach((n) => {
     augmentLogger(n);
   });
+}
+
+// https://stackoverflow.com/a/4819886/5983554
+function is_touch_device() {
+  if ("ontouchstart" in window || window.TouchEvent) return true;
+
+  if (window.DocumentTouch && document instanceof DocumentTouch) return true;
+
+  const prefixes = ["", "-webkit-", "-moz-", "-o-", "-ms-"];
+  const queries = prefixes.map((prefix) => `(${prefix}touch-enabled)`);
+
+  return window.matchMedia(queries.join(",")).matches;
 }
